ae1a92093ce800d4206390a551ce0658d4cb7438
7ch <7ch@gmail.com>
2025-09-04 15:02:26 +0800

    init_git

---
+++ ch1/cincout_test.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+int main(){
+    std::cout << "test cin" << std::endl;
+    int x,y;
+    std::string z;
+    std::cin >> x  >> y >> z ;
+    std::cout << "x: " << x << ", y: " << y << ", z: " << z << std::endl;
+}
